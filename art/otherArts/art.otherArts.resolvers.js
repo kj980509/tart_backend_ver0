be4dba02b4c37dcf74e1cf36350ab9c10cfedaa6
ju-hee <kj980509@naver.com>
2021-10-26 23:01:48 +0900

    tart_backend_ver0

--- art/otherArts/art.otherArts.resolvers.js
+++ art/otherArts/art.otherArts.resolvers.js
@@ -5,40 +5,17 @@ export default {
         otherArts: async (_, {userId, artId, isExist}) => {
 
             // Get Other Arts of User
-            let artIds = await client.art.findMany({
-                where: {userId},
-                select: {id:true}
-            })
-
-            artIds =  artIds.map( (artId) => artId.id  )
-
-            artIds = artIds.filter(function(id) { return id !== artId })
-            console.log(artIds)
-
-            if (artIds.length === 0) {
-                return{
-                    ok: true,
-                    isExist: false
+            const arts = await client.art.findMany({
+                where: {
+                    userId,
+                    NOT:{
+                        id:artId
+                    }
                 }
-            }
-
-            const imageUrls = []
-            for (const artId of artIds){
-                const {imageUrl} =  await client.artPhoto.findFirst({
-                    where:{artId},
-                    select:{imageUrl:true}
-                })
-                imageUrls.push(imageUrl)
-            }
+            })
+            console.log(arts);
+            return arts
 
 
-
-            return{
-                ok:true,
-                imageUrls: imageUrls,
-                ids: artIds
-            }
-
-        }
     }
-}
+}}
--- art/otherArts/art.otherArts.typeDefs.js
+++ art/otherArts/art.otherArts.typeDefs.js
@@ -1,17 +1,8 @@
 import {gql} from "apollo-server-express";
 
 export default gql`
-    
-    type Arts{
-        ok: Boolean!
-        error: String
-        imageUrls: [String]
-        ids: [Int]
-        isExist: Boolean!
-    }
-    
     type Query{
-        otherArts(artId:Int!, userId:Int!): Arts!
+        otherArts(artId:Int!, userId:Int!): [Art]
     }
 
 `
